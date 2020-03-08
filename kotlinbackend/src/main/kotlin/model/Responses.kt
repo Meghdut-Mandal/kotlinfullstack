77b085a6e99c49ad8293e97daa27023d9b76db3d
Meghdut Mandal <meghdut.windows@gmail.com>
2020-03-08 09:29:32 +0530

    Added Json Error Response To Exceptions occuring

---
+++ kotlinbackend/src/main/kotlin/model/Responses.kt
@@ -0,0 +1,7 @@
+package model
+
+data class AbstractAPIResponse<T>(val statusCode: Int, val skip: Int = 0, val limit: Int, val dataList: List<T>)
+data class ErrorResponse(val title: String, val shortMessage: String, val stacktrace: String)
+
+fun Throwable.getErrorResponse() =
+        ErrorResponse(this::class.toString(), this.localizedMessage, this.stackTrace.joinToString(separator = "\n"))
