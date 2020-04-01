c36dce0189946809a369c7c8dec16a9500e6f903
Meghdut Mandal <meghdut.windows@gmail.com>
2020-04-02 01:10:33 +0530

    Adding Teacher sucessfull

--- kotlinbackend/src/main/kotlin/routes/TeacherRout.kt
+++ kotlinbackend/src/main/kotlin/routes/TeacherRout.kt
@@ -1,13 +1,18 @@
 package routes
 
-import TeacherRequest
+import TeacherRequest.SignUpPage
 import dao.TeacherDao
 import io.ktor.application.call
+import io.ktor.http.Parameters
 import io.ktor.locations.get
 import io.ktor.locations.post
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.thymeleaf.ThymeleafContent
+import model.Teacher
+import java.security.MessageDigest
+import java.util.*
 
 
 /*
@@ -21,13 +26,34 @@ class TeacherRequest
     class LogInRequest
 }
  */
+fun hash(string: String): String {
+    val data = string.toByteArray()
+    val digester = MessageDigest.getInstance("SHA-256")
+    digester.update(data)
+    return Base64.getEncoder().encodeToString(digester.digest())
+}
 
 fun Route.teachers(teacherDao: TeacherDao) {
-    get<TeacherRequest.SignUpPage> {
+    get<SignUpPage> {
         call.respond(ThymeleafContent("teacher_signup", mapOf()))
     }
-    post<TeacherRequest.SignUpPage> {
-
-        call.respond("done bro !!")
+    post<SignUpPage> {
+        val post = call.receive<Parameters>()
+        val userName = post["name"]
+                ?: return@post call.respond(ThymeleafContent("teacher_signup", mapOf("error" to "The name Should not be empty")))
+        val userEmail = post["email"]
+                ?: return@post call.respond(ThymeleafContent("teacher_signup", mapOf("error" to "The Email Should not be empty")))
+        val userPassword = post["psw"]
+                ?: return@post call.respond(ThymeleafContent("teacher_signup", mapOf("error" to "The Password Should not be empty")))
+        if (teacherDao.hasTeacher(userEmail)) {
+            return@post call.respond(ThymeleafContent("teacher_signup", mapOf("error" to "The email has been already used. Try some other Email.")))
+        } else {
+            val teacher =
+                    Teacher(userEmail, hash(userEmail + userPassword), userName, arrayListOf())
+            val sucess = teacherDao.addTeacher(teacher)
+            if (sucess)
+                call.respond("Registration done sucessfully for $teacher")
+            else call.respond("Some internal error is there !")
+        }
     }
 }
